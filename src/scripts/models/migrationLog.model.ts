ba940a6c69adb2b144ba9d77caf68855b37e157d
abdullah boshir <96838904+abdullahboshir@users.noreply.github.com>
2025-07-28 00:34:43 +0600

    fixed

--- src/scripts/models/migrationLog.model.ts
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-
-const migrationSchema = new mongoose.Schema({
-  name: { type: String, unique: true },
-  runAt: { type: Date, default: Date.now },
-});
-
-export const Migration = mongoose.model('Migration', migrationSchema);
